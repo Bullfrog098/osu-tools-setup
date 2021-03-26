3208b609d1b3c848a5812a225e552a9d50c390e4
Bullfrog098 <hstohlmaniii@outlook.com>
2021-03-26 12:48:09 -0400

    testing things

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
-const egg = require('./downloader');
+/*const egg = require('./index');
 (async ()=>{
-    const e = await egg.firsttimesetup();
+    const e = await egg.updateosutools();
     console.log(e)
-})();
+})();*/
+const download = require('download-git-repo')
+download('ppy/osu-tools', './osu-tools', function (err) {
+    console.log(err ? 'Error' : 'Success')
+  })
